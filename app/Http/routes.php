a8eaace7b668f70cb7e4dc98136552ed42bfac0b
Edward Hashner Wijaya <edwardhashner@gmail.com>
2016-12-03 03:55:34 +0700

    Perbaikin Route (Alfredo)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,29 +20,27 @@ Route::get('/home', function () {
     return view('home');
 });
 
-<<<<<<< HEAD
-Route::get('admin/search',function(){
+
+Route::get('admin/search',function() {
     $search = urlencode(e(\Illuminate\Support\Facades\Input::get('search')));
     $route = "admin/search_company/$search";
     return redirect($route);
-=======
+});
+
 Route::get('/{user_id}', [
     'uses' => 'ProfileController@index',
 ]);
 
 Route::get('/admin/search_company',function(){
    return view('admin/search_company');
->>>>>>> 3a1b130747efeeaf0c568d38d5e3bccb5781c53e
 });
+
 Route::get('admin/search_company/{search}','AdminController@searchCompany');
 Route::get('admin/view_search','AdminController@index');
 
 Route::group(['middleware' => ['web']], function () {
     //
-
 });
-
-
 
 Route::auth();
 
