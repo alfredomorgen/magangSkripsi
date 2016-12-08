8f628d033173d809a3e6e4b0896dd07c80903539
Edward Hashner Wijaya <edwardhashner@gmail.com>
2016-12-08 21:39:09 +0700

    Job Detail Updated

--- resources/views/job/job_detail.blade.php
+++ resources/views/job/job_detail.blade.php
@@ -12,26 +12,62 @@
     <div class="container">
         <div class="row">
             <div class="col s12 m12 l12">
-                <div class="card white darken-1">
+                <div class="card white darken-1 hoverable">
+                    <div class="card-content orange darken-1 center white-text"><h6>VIEW POST</h6></div>
                     <div class="card-content grey-text text-darken-2" style="padding-left:50px;padding-right:50px;">
-                        <div class="center"><h4 class="">{{ $job->title }}</h4></div><br>
-                        <h5>Company</h5>
-                        <p><a href="{{route('user.index',$job->company->user->id)}}">{!!  nl2br($job->company->user->name) !!}</a></p><br>
-                        <h5>Job Description</h5>
-                        <p>{!! nl2br ($job->description) !!}</p><br>
-                        <h5>Benefit</h5>
-                        <p>{!! nl2br($job->benefit) !!}</p><br>
-                        <h5>Requirement</h5>
-                        <p>{!! nl2br($job->requirement) !!}</p><br>
-                        <h5>Employment Type</h5>
-                        <p>@if($job->type =='0') Full Time @else Part Time @endif</p><br>
-                        @if($job->company->website!= NULL)
-                            <h5>Website</h5>
-                            <p>{!! nl2br($job->company->website) !!}</p>
-                        @endif
+
+                        <div class="right-align">
+                            <a class="grey-text tooltipped right-align" data-position="bottom" data-delay="10" data-tooltip="Deadline Time"><i class="tiny material-icons">schedule </i> {{ $job->created_at}}</a>
+                        </div>
+                        <div class="center"><h4 class="">{{ $job->title }}</h4></div>
+                        <br>
+                        <div class="card-action">
+                            <h5 class="blue-text">Company</h5>
+                            <p>
+                                <i class="tiny material-icons">work</i>
+                                <a href="{{route('user.index',$job->company->user->id)}}"class="yellow-text text-darken-4 tooltipped" data-position="right" data-delay="10" data-tooltip="View Company">{!!  nl2br($job->company->user->name) !!}</a>
+                            </p>
+                        </div>
+                        <div class="card-action">
+                            <h5 class="blue-text">Job Description</h5>
+                            <p>{!! nl2br ($job->description) !!}</p>
+                        </div>
+                        <div class="card-action">
+                            <h5 class="blue-text">Benefit</h5>
+                            <p>{!! nl2br($job->benefit) !!}</p>
+                        </div>
+                        <div class="card-action">
+                            <h5 class="blue-text">Requirement</h5>
+                            <p>{!! nl2br($job->requirement) !!}</p>
+                        </div>
+                        <div class="card-action">
+                            <h5 class="blue-text">Employment Type</h5>
+                            <p><i class="tiny material-icons">av_timer</i>@if($job->type =='0') Full Time @else Part Time @endif</p>
+                        </div>
+                        <div class="card-action">
+                            <h5 class="blue-text">Salary</h5>
+                            <p><i class="tiny material-icons">payment</i> @if($job->type =='0') Paid @else Not Paid @endif</p>
+                        </div>
+                        <div class="card-action">
+                            <h5 class="blue-text"> Period </h5>
+                            <p><i class="tiny material-icons">schedule</i> {!! nl2br($job->period) !!} Months</p>
+                        </div>
+
+                        <div class="card-action">
+                            <h5 class="blue-text">Company Information</h5>
+                            @if($job->company->website!= NULL)
+                                <p><i class="tiny material-icons"> language</i><b> Website :</b> {!! nl2br($job->company->website) !!}</p>
+                            @endif
+                            <p><i class="tiny material-icons">business</i><b> Industry :</b> {!! nl2br($job->company->industry) !!}</p>
+                            <p><i class="tiny material-icons">perm_identity</i><b> Company Size :</b> {!! nl2br($job->company->size) !!} Employees</p>
+
+                        </div>
+
                     </div>
                     <div class="card-action">
-                        <h6 class="right-align"><button class="btn btn-default" id="btnApply">Apply</button></h6>
+                        <h6 class="right-align">
+                            <button class="btn btn-default orange waves-effect" id="btnApply">Apply</button>
+                        </h6>
                     </div>
                 </div>
             </div>
@@ -55,8 +91,8 @@
                 });
             @else
                 $('#btnApply').click(function (event) {
-                    window.location.href += '/apply';
-                });
+                window.location.href += '/apply';
+            });
             @endif
 
             Materialize.toast('{{ session('message') }}', 3000, 'rounded');
