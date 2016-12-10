27db5b2a04007762879d85183ec63ee22173260f
Alfredo Romero Morgen Wiria <Alfredo7Romero@gmail.com>
2016-12-11 02:59:05 +0700

    :memo: Page Title di tab browser
    + Menambahkan title di halaman Home, profil Company dan Jobseeker serta
    di Job Detail

--- resources/views/job/job_detail.blade.php
+++ resources/views/job/job_detail.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', $job->company->user->name.' - '.$job->name)
 @section('content')
     <div class="container">
         <div class="row">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Magang.com') }}</title>
+    <title>{{ config('app.name', 'Magang.com')}} @if(View::hasSection('title')) - @yield('title')@endif</title>
     <!--Import Google Icon Font-->
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
